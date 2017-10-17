7f97fd07d66f9b0b19a160a39f2ede69730725f1
zcac56 <zcac56@163.com>
2017-10-17 19:50:00 +0800

    create swingExample

---
+++ src/swingExample/AngelListener.java
@@ -0,0 +1,11 @@
+package swingExample;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AngelListener implements ActionListener {
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.println("Don't do it, you might regret it!");
+	}
+
+}
